75de3f181faf522643f6964c3908f5c5d8d4d48e
James Lovallo <james.lovallo@thrivent.com>
2024-09-26 11:34:45 -0400

    add newsletter

--- pages/contact.js
+++ pages/contact.js
@@ -9,6 +9,16 @@ export const meta = {
 export const body = md`
 # Contact
 
+## Newsletter
+
+${linkList([
+	{
+		icon: 'EmailNewsletter',
+		title: 'Sign up',
+		href: 'https://actionnetwork.org/forms/subscribe-to-our-newsletter-13?source=direct_link&',
+	},
+])}
+
 ## Social Media
 
 ${linkList(social)}
